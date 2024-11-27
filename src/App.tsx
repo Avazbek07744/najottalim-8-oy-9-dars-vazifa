ca51c98fc4db42e5e44ae3e630b8494084320dab
Avazbek07744 <javazbek87@gmail.com>
2024-11-27 21:37:16 +0500

    9-vazifam

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chart from './components/Chart';
+import Main from './components/Main';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Main />
+      <Chart />
+    </div>
+  );
+};
+
+export default App;
